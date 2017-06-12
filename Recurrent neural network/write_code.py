a25554224f6adc53881f18efcb6df0f93923aec4
adminho <couragor@gmail.com>
2017-06-12 11:20:37 +0700

    edit file

--- Recurrent neural network/write_code.py
+++ Recurrent neural network/write_code.py
@@ -8,30 +8,32 @@ from keras.utils.data_utils import get_file
 import numpy as np
 import random
 
-source_code = open('index.php', encoding="utf8").read().lower()
+#source_code = open('index.html', encoding="utf8").read().lower()
+source_code = open('index.html', encoding="utf8").read()
+
 # I'm split to an array easily (In practise, don't it)
 source_code = source_code.split() # default is space to split 
 print('source code exampe:')
 print(source_code)
-print('corpus length:', len(source_code))
+print('\ncorpus length:', len(source_code))
 
-# to reduce duplicate words
+# to reduce duplicate words (not order)
 tokens = sorted(list(set(source_code)))
-num_tokens = len(tokens)		
-print('total tokens:', num_tokens)
+ENCODING_LEN = len(tokens)		
+print('total tokens:', ENCODING_LEN)
 
 # for converting a token to index
 char_indices = dict((c, i) for i, c in enumerate(tokens))
 # for converting index to a token
 indices_char = dict((i, c) for i, c in enumerate(tokens))
 
-# cut the source_code in semi-redundant sequences of maxlen characters
-maxlen = 2
+# cut the source_code in semi-redundant sequences of MAX_SEQ_LEN characters
+MAX_SEQ_LEN = 2
 batch_seq_tokens = []
 next_tokens = []
-for i in range(0, num_tokens - maxlen):
-    batch_seq_tokens.append(source_code[i: i + maxlen])
-    next_tokens.append(source_code[i + maxlen])
+for i in range(0, len(source_code) - MAX_SEQ_LEN):
+    batch_seq_tokens.append(source_code[i: i + MAX_SEQ_LEN])
+    next_tokens.append(source_code[i + MAX_SEQ_LEN])
 	
 print('len batch_seq_tokens:', len(batch_seq_tokens))
 print('len next_tokens:', len(next_tokens))
@@ -40,36 +42,50 @@ print('Vectorization...')
 # One-hot encoding 
 # batch_seq_tokens is encoded to X
 # next_tokens is encoded to y
-# num_tokens is: the length of a encoded token vector
-X = np.zeros((len(batch_seq_tokens), maxlen, num_tokens), dtype=np.bool)
-y = np.zeros((len(batch_seq_tokens), num_tokens), dtype=np.bool)
+# ENCODING_LEN is: the length of a encoded token vector
+X = np.zeros((len(batch_seq_tokens), MAX_SEQ_LEN, ENCODING_LEN), dtype=np.bool)
+y = np.zeros((len(batch_seq_tokens), ENCODING_LEN), dtype=np.bool)
 for i, seq_tokens in enumerate(batch_seq_tokens):
     for t, token in enumerate(seq_tokens):
         X[i, t, char_indices[token]] = 1
     y[i, char_indices[next_tokens[i]]] = 1
 
-print("X shape", X.shape)
-print("y.shape", y.shape)
+assert X.shape == (24, 2, 22)
+assert y.shape == (24, 22)
 
 # build the model: a single LSTM
 print('Build model...')
 model = Sequential()
-# Input size: (None, 2, 27) = (bath_num, sequences_num, dim_input)
-# Ouput size: (None, 2, 20)
-model.add(LSTM(20, input_shape=(maxlen, num_tokens), return_sequences=True))
-# Ouput size: (None, 20)
+# Input size: (None, 2, 22) = (bath_num, sequences_num, dim_input)
+model.add(LSTM(20, input_shape=(MAX_SEQ_LEN, ENCODING_LEN), return_sequences=True))
 model.add(LSTM(20, return_sequences=False))
-
 # Fully-connected layer
-# Ouput size: (None, 27)
-model.add(Dense(num_tokens))
+model.add(Dense(ENCODING_LEN))
 model.add(Activation('softmax'))
+
 print(model.summary())
+"""_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+lstm_1 (LSTM)                (None, 2, 20)             3440
+_________________________________________________________________
+lstm_2 (LSTM)                (None, 20)                3280
+_________________________________________________________________
+dense_1 (Dense)              (None, 22)                462
+_________________________________________________________________
+activation_1 (Activation)    (None, 22)                0
+=================================================================
+Total params: 7,182
+Trainable params: 7,182
+Non-trainable params: 0
+_________________________________________________________________
+"""
 
 optimizer = RMSprop(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
-def get_index(preds, temperature=1.0):
+# copy this function from: https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
+def get_probIndex(preds, temperature=1.0):
     # helper function to sample an index from a probability array
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
@@ -78,22 +94,32 @@ def get_index(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+# One-hot encoding			  
+def encode_X(sequences_token):
+	sequences_encode = np.zeros((1, MAX_SEQ_LEN, ENCODING_LEN))
+	for t, char in enumerate(sequences_token):    
+		sequences_encode[0, t, char_indices[char]] = 1
+	return sequences_encode
+
 # Train	
-for iteration in range(1, 13):    
-    print('Iteration', iteration)
+for iteration in range(1, 15):    
+    print('Iteration %s\n' % iteration)
     model.fit(X, y,
               batch_size=25,
               epochs=10,verbose=0) # verbose 1 print a progress status
 
-# One-hot encoding			  
-sequences_token = ['</head>', '<body>']
-sequences_encode = np.zeros((1, maxlen, num_tokens))
-for t, char in enumerate(sequences_token):    
-    sequences_encode[0, t, char_indices[char]] = 1
-				
-preds = model.predict(sequences_encode, verbose=0)[0]
-next_index = get_index(preds)
-next_char = indices_char[next_index]
+# for begining			  
+generate ="<html> <head>"
+sequences_token = generate.split()
+print("Tokens for begining:")
+print(sequences_token) # ['<html>', '<head>']
 
-print('Tokens list:', sequences_token)
-print('Answer:',  next_char)
+for i in range(0, len(source_code) - MAX_SEQ_LEN):    
+	sequences_encode = encode_X(sequences_token)	
+	preds = model.predict(sequences_encode, verbose=0)[0]
+	next_index = get_probIndex(preds)
+	next_char = indices_char[next_index]
+	generate += ' ' + next_char
+	sequences_token = np.append(sequences_token[1:],next_char)         
+
+print('Generate text:\n', generate)
