0d3d27cc0e3b608d4eb458778fa82e56bd8ac983
Administrator <couragor@gmail.com>
2017-07-16 16:45:59 -0700

    edit

--- Activation_function/activation_function.py
+++ Activation_function/activation_function.py
@@ -9,24 +9,32 @@ def tanh(x):
 	return 2 * sigmoid(2*x) - 1
 
 def relu(x):
-    return x *(x>=0) 
-    
+	return x *(x>=0) 
+	
+def leaky_relu(X, leak=0.2):
+	f1 = 0.5 * (1 + leak)
+	f2 = 0.5 * (1 - leak)
+	return f1 * X + f2 * np.abs(X)
+
 x = np.arange(-50,50,0.1)	
 y1 = sigmoid(x)
 y2 = tanh(x)
 y3 = relu(x)
+y4 = leaky_relu(x)
 
 # create 4 graph
 fig = plt.figure()
-ax1 = fig.add_subplot(221)
-ax2 = fig.add_subplot(222)
-ax3 = fig.add_subplot(223)
-ax4 = fig.add_subplot(224)
+ax1 = fig.add_subplot(231)
+ax2 = fig.add_subplot(232)
+ax3 = fig.add_subplot(233)
+ax4 = fig.add_subplot(234)
+ax5 = fig.add_subplot(235)
 
 ax1.title.set_text('Input')
 ax2.title.set_text('Sigmoid function')
 ax3.title.set_text('TanH function')
 ax4.title.set_text('ReLU function')
+ax5.title.set_text('Leaky ReLU function')
 
 # set scale on y axis
 ax2.set_ylim([-0.1, 1.1])
@@ -43,10 +51,13 @@ diplay_xy_axis(ax1)
 diplay_xy_axis(ax2)
 diplay_xy_axis(ax3)
 diplay_xy_axis(ax4)
+diplay_xy_axis(ax5)
 
 # show all graphs
 ax1.plot(x, x,'k-')
 ax2.plot(x, y1,'r-')
 ax3.plot(x, y2,'g-')
 ax4.plot(x, y3,'b-')
+ax5.plot(x, y4,'c-')
+plt.tight_layout()
 plt.show()
