a3d340bfb14c50a02a725be9b6bd47c5212a9f22
couragor@gmail.com <meho7931op>
2018-11-04 22:04:47 +0700

    edit files

--- Recurrent_neural_network/writer.py
+++ Recurrent_neural_network/writer.py
@@ -1,4 +1,4 @@
-# code reference from Keras example : https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
+# My idea from this example : https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM, GRU
@@ -7,16 +7,6 @@ from keras.optimizers import RMSprop
 import os.path
 import shutil	
 import numpy as np
-
-# copy this function from: https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
-def _get_probIndex(preds, temperature=1.0):
-	# helper function to sample an index from a probability array
-	preds = np.asarray(preds).astype('float64')
-	preds = np.log(preds) / temperature
-	exp_preds = np.exp(preds)
-	preds = exp_preds / np.sum(exp_preds)
-	probas = np.random.multinomial(1, preds, 1)
-	return np.argmax(probas)
 
 # For example 1
 def build_model1(max_seqlen, encoding_len):
@@ -69,6 +59,41 @@ TEMP_PATH = 'temp'
 if os.path.exists(TEMP_PATH):
 	shutil.rmtree(TEMP_PATH)	
 os.makedirs(TEMP_PATH)
+
+class Vocabulary():
+	def __init__(self, tokens):
+		# Vocabulary
+		# for converting all tokens to index
+		self.token2indices = dict((token, i) for i, token in enumerate(tokens))
+		# for converting all index to a tokens
+		self.indices2token = dict((i, token) for i, token in enumerate(tokens))
+		self.encoding_len = len(tokens)	
+
+	def encode_target(self, next_tokens):
+		# One-hot encoding		
+		Y_encoded = np.zeros((len(next_tokens), self.encoding_len))	
+		for i, tokens in enumerate(next_tokens):		
+			Y_encoded[i, self.token2indices[tokens]] = 1
+		return Y_encoded
+	
+	def encode_input(self, batch_tokens):
+		# One-hot encoding
+		total, seq_len = np.shape(batch_tokens)		
+		X_encoded = np.zeros((total, seq_len, self.encoding_len))
+		for i, tokens in enumerate(batch_tokens):
+			for t, token in enumerate(tokens):
+				X_encoded[i, t, self.token2indices[token]] = 1
+		return X_encoded
+	
+	# copy this function from: https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
+	def decode_predict(self, preds, temperature=1.0):
+		# helper function to sample an index from a probability array
+		preds = np.asarray(preds).astype('float64')
+		preds = np.log(preds) / temperature
+		exp_preds = np.exp(preds)
+		preds = exp_preds / np.sum(exp_preds)
+		probas = np.random.multinomial(1, preds, 1)
+		return np.argmax(probas)
 	
 def get_trainer(content, tokens, max_seqlen, build_model, step=1):
 	# clear up tokens duplicated
@@ -77,35 +102,24 @@ def get_trainer(content, tokens, max_seqlen, build_model, step=1):
 	print("\ntokens:\n", tokens)
 	print("\ntotal tokens: ", encoding_len)
 	print("sequences length: ", max_seqlen)
+		
+	vocab = Vocabulary(tokens)
 	
-	# Vocabulary
-	# for converting a token to index
-	token_indices = dict((token, i) for i, token in enumerate(tokens))
-	# for converting index to a token
-	indices_token = dict((i, token) for i, token in enumerate(tokens))
-
+	print('Preparing the input and target...')
+	# Preparing the input and target
 	# cut the content in semi-redundant sequences of max_seqlen characters
 	batch_seqtokens = []
 	next_tokens = []
 	for i in range(0, len(content) - max_seqlen, step):
 		batch_seqtokens.append(content[i: i + max_seqlen])
-		next_tokens.append(content[i + max_seqlen])
-	
+		next_tokens.append(content[i + max_seqlen])	
 	print('len batch_seqtokens:', len(batch_seqtokens))
 	print('len next_tokens:', len(next_tokens))
 
 	print('Vectorization...')
 	# One-hot encoding 
-	# batch_seqtokens => encoded to X
-	# next_tokens => encoded to y
-	# encoding_len => the length of a encoded token vector
-	X = np.zeros((len(batch_seqtokens), max_seqlen, encoding_len))
-	y = np.zeros((len(batch_seqtokens), encoding_len))
-	for i, seq_tokens in enumerate(batch_seqtokens):
-		for t, token in enumerate(seq_tokens):
-			X[i, t, token_indices[token]] = 1
-		y[i, token_indices[next_tokens[i]]] = 1
-
+	X = vocab.encode_input(batch_seqtokens)
+	y = vocab.encode_target(next_tokens)	
 	print("X shape:", X.shape)
 	print("Y shape:", y.shape)
 
@@ -138,12 +152,11 @@ def get_trainer(content, tokens, max_seqlen, build_model, step=1):
 			
 			for i in range(0, len(content) - max_seqlen):	
 				# One-hot encoding
-				seq_encode = np.zeros((1, max_seqlen, encoding_len))		
-				for t, token in enumerate(seq_tokens):				
-					seq_encode[0, t, token_indices[token]] = 1	
-				preds = model.predict(seq_encode, verbose=0)[0] # Output shape is [1, encoding_len]
-				next_index = _get_probIndex(preds, diversity)
-				next_char = indices_token[next_index]			
+				seq_encoded = vocab.encode_input([seq_tokens]) # input shape is [1, number of tokens, encoding_len ]				
+				preds = model.predict(seq_encoded, verbose=0)[0] # Output shape is [1, encoding_len]
+				
+				next_index = vocab.decode_predict(preds, diversity)
+				next_char = vocab.indices2token[next_index]			
 				generate_text = np.append(generate_text, next_char)
 				seq_tokens = np.append(seq_tokens[1:], next_char)				
 			
