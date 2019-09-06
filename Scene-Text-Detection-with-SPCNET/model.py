330b8de4c3214adf13070419eed4afcff29ed8b1
adminho <couragor@gmail.com>
2019-09-07 03:21:40 +0700

    Update model.py

--- Scene-Text-Detection-with-SPCNET/model.py
+++ Scene-Text-Detection-with-SPCNET/model.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import tensorflow as tf
 from tensorflow.contrib import slim
 from nets import utils
-import resnet_v1
+from nets import resnet_v1
 import numpy as np
 import sys
 
